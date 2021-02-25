b224598cf52ec4faab071407e81a9409068f6465
Pezu-git <artem_nikolaev69@mail.ru>
2021-02-25 09:44:48 +0300

    first

--- src/js/Character.js
+++ src/js/Character.js
@@ -4,9 +4,18 @@ export default class Character {
     this.name = name;
     this.type = type;
     if (this.name.length <= 2 || this.name.length > 10) {
-      throw new Error('Не подходящее имя');
+      this.getError('Не подходящее имя');
     } if (this.type !== 'Daemon' && this.type !== 'Bowerman' && this.type !== 'Swordsman' && this.type !== 'Magician' && this.type !== 'Undead' && this.type !== 'Zombie') {
-      throw new Error('Не подходящее имя класса');
+      this.getError('Не подходящее имя класса');
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getError(message) {
+    try {
+      throw new Error(message);
+    } catch {
+      console.error(message);
     }
   }
 }
